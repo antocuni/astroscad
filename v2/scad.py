e290864de99403738bc00b64007758d4ceb95f7b
Antonio Cuni <anto.cuni@gmail.com>
2021-04-03 01:19:16 +0200

    add support for negative dimensions

--- v2/scad.py
+++ v2/scad.py
@@ -1,23 +1,29 @@
 import solid
 
-def cube(sx, sy, sz, center=''):
+def _get_st(axis, size, center):
+    assert axis in ('x', 'y', 'z')
+    t = 0
+    if size < 0:
+        size = -size
+        t = -size
+    if axis in center:
+        t = -size/2
+    return size, t
+
+def _process_center(center):
     assert center in ('', 'x', 'y', 'z', 'xy', 'xz', 'yz', 'xyz', True, False)
     if center is True:
-        center = 'xyz'
+        return 'xyz'
     if center is False:
-        center = ''
+        return ''
+    return center
 
-    tx, ty, tz = 0, 0, 0
-    if center == 'xyz':
-        return solid.cube([sx, sy, sz], center=True)
 
-    if 'x' in center:
-        tx = -sx/2
-    if 'y' in center:
-        ty = -sy/2
-    if 'z' in center:
-        tz = -sz/2
-
+def cube(sx, sy, sz, center=''):
+    center = _process_center(center)
+    sx, tx = _get_st('x', sx, center)
+    sy, ty = _get_st('y', sy, center)
+    sz, tz = _get_st('z', sz, center)
     return solid.translate([tx, ty, tz])(
         solid.cube([sx, sy, sz]),
     )
