ba41786d51b99801bf73bea2d5180739f017e1c3
Antonio Cuni <anto.cuni@gmail.com>
2021-11-08 23:04:35 +0100

    add a test to show spur and worm together

--- v2/pyscad/test/test_gears.py
+++ v2/pyscad/test/test_gears.py
@@ -19,3 +19,37 @@ class TestGear(OpenSCADTest):
         obj += Cylinder(r=worm.r, h=worm.length).color('red', 0.3).rot(x=90)
         obj += Cube(worm.r, worm.length, 2).color('green', 0.3).move_to(bottom=worm.top)
         self.check(obj)
+
+    def test_bracket(self):
+        R_WORM = 5.75877
+        def bracket():
+            r_worm = R_WORM
+            r_gear = 12
+            t = 4 # thickness*2
+            w = 28
+            h = 2.5
+            obj = Cube(w+t, w+t,  h+t)
+            obj -= Cube(w+t+1, w, 2.5)
+            obj -= Cube(w+t+1, w, h+t+1).translate((w+t+1)/2)
+            #
+            delta = -w/4
+            obj -= (
+                bolt_hole(d=3.2, h=h+t)
+                .tr(x=delta)
+                )
+            obj -= (
+                bolt_hole(d=3.2, h=50)
+                .rot(x=90)
+                .tr(delta+r_gear+r_worm)
+                )
+            return obj.tr(x=-delta)
+
+        root = Union()
+        root += bracket().mod('%')
+        spur = WormFactory.spur(teeth=24, h=2, bore_d=3.2)
+        worm = WormFactory.worm(length=15, bore_d=0)
+        root += spur
+        root += worm.move_to(left=spur.right)
+        self.check(root)
+
+
