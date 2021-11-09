b1328b1e6f75906fbd0921ca3bea5399c8f84de3
Antonio Cuni <anto.cuni@gmail.com>
2021-11-09 18:41:28 +0100

    add a Teflon Glide

--- v2/pyscad/lib/misc.py
+++ v2/pyscad/lib/misc.py
@@ -1,6 +1,30 @@
-from ..scad import Cylinder, EPS
+from ..geometry import Point
+from ..scad import Cylinder, EPS, CustomObject
 
 def ring(outer_d, inner_d, h):
     result = Cylinder(d=outer_d, h=h)
     result -= Cylinder(d=inner_d, h=h+EPS)
     return result
+
+
+class TeflonGlide(CustomObject):
+    # https://www.amazon.com/dp/B07FH82D8J
+    # measures took manually
+
+    def init_custom(self):
+        self.d = 19
+        self.inner_d = 9.20
+        self.hole_d = 4.30
+        self.base_h = 3.5
+        self.upper_h = 1.9
+        self.h = self.base_h + self.upper_h
+        self._base = ring(self.d, self.hole_d, self.base_h)\
+            .color([0.3, 0.3, 0.3])\
+            .move_to(bottom=Point.O)
+        self._upper = ring(self.d, self.inner_d, self.upper_h)\
+            .move_to(bottom=self._base.top-EPS)\
+            .color('LightSteelBlue')
+        #
+        self.translate(z = -self.h/2)
+        self.anchors.set_bounding_box(self._base.pmin, self._upper.pmax)
+        self.anchors.center = Point.O
--- v2/pyscad/test/test_lib.py
+++ v2/pyscad/test/test_lib.py
@@ -1,5 +1,6 @@
-from pyscad import Cube, Cylinder, ImportScad, bolt_hole, Union, Point
+from pyscad import Cube, Cylinder, ImportScad, bolt_hole, Union, Point, CustomObject
 from pyscad.lib.bearing import Bearing
+from pyscad.lib.misc import TeflonGlide
 from .test_render import OpenSCADTest
 
 
@@ -9,4 +10,11 @@ class TestLib(OpenSCADTest):
         obj = Union()
         obj += Bearing('608')
         obj += Bearing('608').translate(x=25).show_bounding_box()
+        self.check(obj, distance=200)
+
+    def test_teflon_glide(self):
+        obj = CustomObject()
+        obj.glide1 = TeflonGlide()
+        obj.glide2 = TeflonGlide().translate(x=25).show_bounding_box()
+        obj.cyl = Cylinder(d=obj.glide1.d, h=obj.glide1.h).translate(x=-25)
         self.check(obj, distance=200)
