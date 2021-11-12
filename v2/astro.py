ed2ba992c1cac4a1aff6b269b7b0bf0c5576013a
Antonio Cuni <anto.cuni@gmail.com>
2021-11-12 18:03:12 +0100

    attacch a manfrotto plate to the baseplate

--- v2/astro.py
+++ v2/astro.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
+import math
 import os
 from pyscad import (Cube, Cylinder, Sphere, bolt_hole, Point, Union, CustomObject, EPS,
                     TCone)
 from pyscad.lib.misc import TeflonGlide
 from pyscad.lib.bearing import Bearing
 from pyscad.lib.gears import WormFactory
+from pyscad.lib.photo import Manfrotto_200PL
 from pyscad import autorender
 
 VITAMINS = True
@@ -26,25 +28,41 @@ class BasePlate(CustomObject):
 
     def init_custom(self):
         bearing = Bearing('608')
-        self.d = 80
-        self.h = bearing.h + self.BEARING_RIM
-        self.base = TCone(d1=bearing.d+5, d2=self.d, h=self.h).color('SandyBrown')
-        self -= bearing.hole(h=self.h+EPS)\
-            .move_to(top=self.base.top - self.BEARING_RIM)
-        self -= Cylinder(d=bearing.d-5, h=self.h+EPS)
+        photo_plate = Manfrotto_200PL()
+
+        # the smaller d must be large enough to cover the whole photo plate,
+        # the larger d must be large enough to cover the spur
+        pp_dim = photo_plate.pmax - photo_plate.pmin
+        self.d1 = math.hypot(pp_dim.x, pp_dim.y)
+        self.d2 = 80
+
+        # the cone must contain the bearing, plus an upper rim which prevents
+        # the bearing from "falling up"
+        h = bearing.h + self.BEARING_RIM
+
+        self.body = TCone(d1=self.d1, d2=self.d2, h=h).color('SandyBrown')
+        self.photo_plate = photo_plate.move_to(top=self.body.bottom).color('grey', 0.5)
+
+        # big hole where to put the bearing. h=100 means "very long"
+        self -= bearing.hole(h=100)\
+            .move_to(top=self.body.top - self.BEARING_RIM)
+
+        # smaller hole to make the bearing accessible from the top
+        self -= Cylinder(d=bearing.d-5, h=100)
         if VITAMINS:
-            self.bearing = bearing.move_to(bottom=self.base.bottom)
-        self.anchors.copy_from(self.base.anchors)
+            self.bearing = bearing.move_to(bottom=self.body.bottom)
+
 
 def main():
     obj = CustomObject()
     obj.baseplate = BasePlate()
-    obj.spur = WormFactory.spur(teeth=70, h=4, optimized=False).move_to(bottom=obj.baseplate.top+5)
+    obj.spur = WormFactory.spur(teeth=70, h=4, optimized=False)\
+                          .move_to(bottom=obj.baseplate.body.top+5)
     obj.worm = WormFactory.worm(length=15, bore_d=4)\
                           .move_to(center=obj.spur.center, left=obj.spur.right)
 
     if VITAMINS:
-        obj.ball_head = BallHead().move_to(bottom=obj.baseplate.top + 20)
+        obj.ball_head = BallHead().move_to(bottom=obj.baseplate.body.top + 20)
     return obj
 
 if __name__ == '__main__':
