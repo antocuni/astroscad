8e744eafe94e164e92722c854d97fe8d6bba9b4b
Antonio Cuni <anto.cuni@gmail.com>
2023-02-07 23:08:07 +0100

    (antocuni, andremrl) model the DSUB connector

--- v2/board_enclosure.py
+++ v2/board_enclosure.py
@@ -10,6 +10,23 @@ from pyscad.util import in2mm
 import astro
 from astro import main, check_almost_equal
 
+def dsub_connector():
+    # this is the male connector
+    dsub = CustomObject()
+    flange = Cube(1.2, 30.8, 12.5)
+    dsub.flange = flange
+    dsub.inside = Cube(8.45+4, 19.3, 10.75).move_to(left=flange.right)
+    dsub.outside = Cube(5.9, 18.1, 9.45).move_to(right=flange.left)
+    dist = (28.7-3)/2 # distance between the center of the hole and the center of the flange
+    dsub -= Cylinder(d=3, h=10, axis='x').move_to(center=flange.center).tr(y=dist)
+    dsub -= Cylinder(d=3, h=10, axis='x').move_to(center=flange.center).tr(y=-dist)
+    dsub.anchors.flange_right = flange.right
+    dsub.anchors.flange_left = flange.left
+    dsub.anchors.flange_front = flange.front
+    dsub.anchors.flange_back = flange.back
+    dsub.anchors.flange_bottom = flange.bottom
+    return dsub
+
 def board():
     res = CustomObject()
     base = Cube(102, 88, 1.6).color('tan')
@@ -48,6 +65,12 @@ def board():
 
     screw_hole2 = RoundHole(d=3.75, h=25)
     res -= screw_hole2.move_to(back=base.back-31, right=base.right-12.5)
+
+    dsub = dsub_connector()
+    dsub.move_to(flange_right=base.left, flange_front=base.front, flange_bottom=base.bottom)
+    dsub.tr(x=-13.5, z=5)
+
+    res.dsub = dsub
 
     return res
 
